0d4ddbd938e4cc2b55f7d0976d9c64cb888fb90c
cedric <cedric@localhost>
2024-03-13 13:04:16 +0100

    Fix eslint for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:jest/all', 'plugin:unicorn/all'],
+  extends: ['eslint:recommended', 'plugin:jest/all', 'plugin:unicorn/all', 'plugin:prettier/recommended'],
   ignorePatterns: ['.eslintrc.js', 'prettier.config.js', 'jest.config.js', 'dotenv.config.ts'],
   settings: { 'import/resolver': { typescript: true, node: true } },
   rules: {
